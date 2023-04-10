e49efa7bc6d40636bc4d50dc114c34267bcc4987
VitorMendonca62 <vitorqueiroz325@gmail.com>
2023-04-10 16:05:45 -0300

    .

--- src/server.js
+++ src/server.js
@@ -1,13 +1,6 @@
-const { server } = require('./app.js');
+const { server, app } = require('./app.js');
 const PORT_SOCKET = 4004;
 const PORT_SERVER = 4003;
-
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('aa');
-});
 
 server.listen(PORT_SOCKET, () =>
   console.log(
