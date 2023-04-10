9e8b470b88524f5f30a4115d8a6353ec577a3499
VitorMendonca62 <vitorqueiroz325@gmail.com>
2023-04-10 15:12:25 -0300

    .

--- src/app.js
+++ src/app.js
@@ -15,6 +15,9 @@ const users = [];
 const messages = [];
 let stateVideo = {};
 
+const cors = require('cors');
+app.use(cors());
+
 io.on('connection', (socket) => {
   console.log(`[SOCKET] => A user connected: ${socket.id}`);
   socket.on('select-name', (data) => {
